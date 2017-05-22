2435040abc5d556de935174a1c931b2db20aa617
IsyakPutra <isaptraputra@gmail.com>
2017-05-22 17:35:34 +0700

    ppm 1.0

---
+++ application/views/kelas_form.php
@@ -0,0 +1,23 @@
+<!-- Main content -->
+        <section class='content'>
+          <div class='row'>
+            <div class='col-xs-12'>
+              <div class='box'>
+                <div class='box-header'>
+                
+                  <h3 class='box-title'>KELAS</h3>
+                      <div class='box box-primary'>
+        <form action="<?php echo $action; ?>" method="post"><table class='table table-bordered'>
+	    <tr><td>Kelas <?php echo form_error('kelas') ?></td>
+            <td><input type="text" class="form-control" name="kelas" id="kelas" placeholder="Kelas" value="<?php echo $kelas; ?>" />
+        </td>
+	    <input type="hidden" name="id_kelas" value="<?php echo $id_kelas; ?>" /> 
+	    <tr><td colspan='2'><button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
+	    <a href="<?php echo site_url('kelas') ?>" class="btn btn-default">Cancel</a></td></tr>
+	
+    </table></form>
+    </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </section><!-- /.content -->
---
+++ application/views/universitas_form.php
@@ -0,0 +1,23 @@
+<!-- Main content -->
+        <section class='content'>
+          <div class='row'>
+            <div class='col-xs-12'>
+              <div class='box'>
+                <div class='box-header'>
+                
+                  <h3 class='box-title'>UNIVERSITAS</h3>
+                      <div class='box box-primary'>
+        <form action="<?php echo $action; ?>" method="post"><table class='table table-bordered'>
+	    <tr><td>Universitas <?php echo form_error('universitas') ?></td>
+            <td><input type="text" class="form-control" name="universitas" id="universitas" placeholder="Universitas" value="<?php echo $universitas; ?>" />
+        </td>
+	    <input type="hidden" name="id_universitas" value="<?php echo $id_universitas; ?>" /> 
+	    <tr><td colspan='2'><button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
+	    <a href="<?php echo site_url('universitas') ?>" class="btn btn-default">Cancel</a></td></tr>
+	
+    </table></form>
+    </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </section><!-- /.content -->
